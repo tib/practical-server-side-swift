26f476d6a4a703b8cdce22ff364cf02dd69e5189
Tibor Bodecs <mail.tib@gmail.com>
2021-12-30 22:13:15 +0100

    blog index

--- Chapter 03/myProject/Sources/App/Modules/Blog/Controllers/BlogFrontendController.swift
+++ Chapter 03/myProject/Sources/App/Modules/Blog/Controllers/BlogFrontendController.swift
@@ -3,9 +3,9 @@ import Vapor
 struct BlogFrontendController {
     
     var posts: [BlogPost] = {
-        stride(from: 0, to: 10, by: 1).map { index in
-            BlogPost(title: "Sample blog post #\(index)",
-                     slug: "sample-blog-post-\(index)",
+        stride(from: 1, to: 9, by: 1).map { index in
+            BlogPost(title: "Sample post #\(index)",
+                     slug: "sample-post-\(index)",
                      image: "/img/posts/\(String(format: "%02d", index + 1)).jpg",
                      excerpt: "Lorem ipsum",
                      date: Date().addingTimeInterval(-Double.random(in: 0...(86400 * 60))),
